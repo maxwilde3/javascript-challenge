a07963f2a61197886eddd69f11cf9fa386dd06d8
maxwilde3 <60453125+maxwilde3@users.noreply.github.com>
2020-07-15 13:58:31 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+const tbody = d3.select("tbody");
+
+function buildTable(data) {
+    tbody.html("");
+
+    data.forEach((dataRow) => {
+        const row = tboday.append("tr");
+
+        Object.values(dataRow).forEach((val)) => {
+            let cell = row.append("td");
+                cell.text(val);
+            }
+        );
+    });
+}
+
+function handleClick() {
+
+    const date = d3.select("#datetime").property("value");
+    let filterData = tableData;
+
+    if(date) {
+
+        filteredData = filteredData.filter(row => row.datetime === date)
+    }
+
+    buildTable(filteredData);
+}
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
